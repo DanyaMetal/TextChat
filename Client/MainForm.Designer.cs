28cb8da89ce0cda5d8a3e35409beb07019ac6b48
DanyaMetaLL <hilyk.d@mail.ru>
2018-08-27 01:39:17 +0300

    Client migration

---
+++ Client/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.подключитьсяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.подключитьсяToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1144, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // подключитьсяToolStripMenuItem
+            // 
+            this.подключитьсяToolStripMenuItem.Name = "подключитьсяToolStripMenuItem";
+            this.подключитьсяToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.подключитьсяToolStripMenuItem.Text = "Подключиться";
+            this.подключитьсяToolStripMenuItem.Click += new System.EventHandler(this.подключитьсяToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(299, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(794, 334);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(299, 380);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(794, 69);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 39);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 483);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem подключитьсяToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Client/MainForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        //Создаём сокет для клиента        
+        static Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        Thread clientTH;
+
+        private void button1_Click(object sender, EventArgs e)
+        {                               
+            sendMsg(textBox1.Text + ": " + richTextBox2.Text);
+        }
+
+        private void подключитьсяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+
+            //Подключаемся к серверу...
+            clientSocket.Connect("127.0.0.1", 904);
+            
+            
+            //Посылаем на сервер имя подключающегося клиента
+            byte[] nameBufer = new byte[1024];
+            nameBufer = Encoding.Unicode.GetBytes(textBox1.Text);
+            clientSocket.Send(nameBufer);
+
+            while(1==1)
+            {
+                byte[] historyBuffer = new byte[1024];
+                clientSocket.Receive(historyBuffer);
+                string historyMsg = Encoding.Unicode.GetString(historyBuffer);
+                historyMsg = historyMsg.Trim('\0');
+
+                if ( historyMsg == "Last")
+                {
+                    sendMsg("OK");
+                    break;
+                }
+
+                else
+                {
+                    richTextBox1.Text += historyMsg;
+                    sendMsg("OK");
+                }
+            }
+
+           // sendMsg("A");
+
+            //Принимаем ответ от сервера об успешном подключении к серверу
+            byte[] answerServer = new byte[1024];
+            clientSocket.Receive(answerServer);
+            richTextBox1.Text += Encoding.Unicode.GetString(answerServer);            
+            
+            //Создаем отдельный поток для постоянной прослушки сервера на принятие сообщений от него
+            clientTH = new Thread(new ThreadStart(doChat));
+            clientTH.Start();
+            
+        }
+
+        //Осуществляет постоянную прослушка на принятие сообщений от сервера
+        private void doChat()
+        {                   
+            while(1==1)
+            { 
+                        
+                    byte[] buffer = new byte[1024];
+                    clientSocket.Receive(buffer);
+                    string listenMsg =  Encoding.Unicode.GetString(buffer);
+
+                //Осуществляем доступ к элементу управления, который был создан в другом потоке
+                richTextBox1.Invoke((Action)delegate { richTextBox1.Text += listenMsg; });
+                
+            }
+        }
+
+        //Посылает сообщение на сервер
+        private void sendMsg(string _msg)
+        {
+            try
+            {
+                //Создаём буфер для хранения кодированных данных и переносим в него эти данные
+                byte[] bufer = new byte[1024];
+                bufer = Encoding.Unicode.GetBytes(_msg);
+                //Посылаем на сервер данные
+                clientSocket.Send(bufer);
+            }
+           
+            catch(Exception ex)
+            {
+                return;
+            }
+        }
+          
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Сообщаем серверу об отключении клиента
+            //sendMsg(textBox1.Text);
+          //  byte[] aanswerServer = new byte[1024];
+           /*
+            //Завершаем поток, освобождаем ресурсы, закрываем сокет подключение и выходим из приложения            
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientTH.Abort();
+            */
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Сообщаем серверу об отключении клиента
+            sendMsg(textBox1.Text);
+            byte[] aanswerServer = new byte[1024];
+
+            //Завершаем поток, освобождаем ресурсы, закрываем сокет подключение и выходим из приложения            
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientTH.Abort();
+            clientSocket.Close();
+            Application.Exit();
+        }
+    }
+}
